66965db72c2906feea3163980b839f1935c43ffd
Mysterio-O <skrabbi.019@gmail.com>
2025-11-26 19:22:18 +0600

    added config

---
+++ basic server with raw node/dist/config/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config({ path: path_1.default.join(process.cwd(), '.env') });
+// console.log(process.cwd())
+// console.log(process.env)
+const config = {
+    env: process.env.NODE_ENV ? Number(process.env.NODE_ENV) : 5000,
+    port: process.env.port
+};
+exports.default = config;
--- basic server with raw node/src/config/index.ts
+++ basic server with raw node/src/config/index.ts
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
 import path from 'path';
 
-dotenv.config({path:path.join(process.cwd(),'.env')});
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
+// console.log(process.cwd())
+// console.log(process.env)
+
+const config = {
+    env: process.env.NODE_ENV ? Number(process.env.NODE_ENV) : 5000,
+    port: process.env.port
+}
+
+export default config
