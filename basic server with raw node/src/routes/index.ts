18a812ae7911afac91a618e2a13ca7dc5663d057
Mysterio-O <skrabbi.019@gmail.com>
2025-11-27 19:57:10 +0600

    added filedb

--- basic server with raw node/src/routes/index.ts
+++ basic server with raw node/src/routes/index.ts
@@ -1,3 +1,4 @@
+import { readUsers, writeUsers } from "../helpers/fileDB";
 import parseBody from "../helpers/parseBody";
 import addRoutes from "../helpers/RouteHandler"
 import sendJson from "../helpers/sendJson"
@@ -17,15 +18,44 @@ addRoutes("GET", '/api', (req, res) => {
     })
 })
 
-addRoutes('POST', '/api/users', async (req, res) => {
-    try {
-        const body = await parseBody(req);
-        sendJson(res, 200, body);
-    } catch (err) {
-        console.error("error", err);
-        sendJson(res, 400, {
-            message: "failed",
-            status: 400
-        })
-    }
-})
+// addRoutes('POST', '/api/users', async (req, res) => {
+//     try {
+//         const body = await parseBody(req);
+
+//         const users = readUsers();
+
+//         const newUser = {
+//             id: Date.now(),
+//             ...body
+//         };
+
+//         users?.push(newUser);
+
+//         writeUsers(newUser)
+
+//         sendJson(res, 200, body);
+//     } catch (err) {
+//         console.error("error", err);
+//         sendJson(res, 400, {
+//             message: "failed",
+//             status: 400
+//         })
+//     }
+// })
+
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+
+  // user json read
+  const users = readUsers();
+
+  const newUser = {
+    ...body,
+  };
+
+  users?.push(newUser);
+
+  writeUsers(users);
+
+  sendJson(res, 201, { success: true, data: body });
+});
