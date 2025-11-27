ad2e4753e75ae489c5b4d1d00dd661b4d8940c8e
Mysterio-O <skrabbi.019@gmail.com>
2025-11-27 18:42:19 +0600

    added handler

--- basic server with raw node/src/helpers/RouteHandler.ts
+++ basic server with raw node/src/helpers/RouteHandler.ts
@@ -1,10 +1,11 @@
 import { IncomingMessage, ServerResponse } from "http";
 
-type RouteHandler = (req:IncomingMessage,res:ServerResponse) => void;
+export type RouteHandler = (req:IncomingMessage,res:ServerResponse) => void;
 
-const routes: Map<string,Map<string,RouteHandler>> = new Map();
+export const routes: Map<string,Map<string,RouteHandler>> = new Map();
 
 function addRoutes(method:string,path:string,handler:RouteHandler){
+    console.log('entered handler')
     if(!routes.has(method)) routes.set(method,new Map());
 
     routes.get(method)!.set(path,handler);
--- basic server with raw node/src/server.ts
+++ basic server with raw node/src/server.ts
@@ -1,57 +1,93 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
 import config from './config';
+import addRoutes, { RouteHandler, routes } from './helpers/RouteHandler';
+
+
+addRoutes("GET", "/", (req, res) => {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(
+        JSON.stringify({
+            message: 'hello',
+            path: req.url
+        })
+    )
+})
 
 const server: Server = http.createServer(
     (req: IncomingMessage, res: ServerResponse) => {
         console.log('server is running...');
 
-        if (req.url == '/' && req.method == 'GET') {
-            res.writeHead(200, { 'content-type': 'application/json' });
-            res.end(
-                JSON.stringify({
-                    message: 'hello',
-                    path: req.url
-                })
-            )
-        };
+        const method = req.method?.toUpperCase() || '';
+        const path = req.url || '';
 
-        if (req.url == '/api' && req.method == 'GET') {
-            res.writeHead(200, { 'content-type': "application/json" });
-            res.end(
-                JSON.stringify({
-                    message: "api route",
-                    path: req.url
-                })
-            )
+        const methodMap = routes.get(method)
+        const handler: RouteHandler | undefined = methodMap?.get(path);
+
+        console.log(`
+            method: ${method},
+            path: ${path},
+            methodMap: ${methodMap},
+            handler: ${handler}
+            `)
+
+        if (handler) {
+            handler(req, res)
+        } else {
+            res.writeHead(404, { 'content-type': "application/json" });
+            res.end(JSON.stringify({
+                message: "not found",
+                success: false,
+                path
+            }))
         }
 
-        if (req.url == '/api/user' && req.method == "POST") {
+        // if (req.url == '/' && req.method == 'GET') {
+        //     res.writeHead(200, { 'content-type': 'application/json' });
+        //     res.end(
+        //         JSON.stringify({
+        //             message: 'hello',
+        //             path: req.url
+        //         })
+        //     )
+        // };
 
-            let body = '';
+        // if (req.url == '/api' && req.method == 'GET') {
+        //     res.writeHead(200, { 'content-type': "application/json" });
+        //     res.end(
+        //         JSON.stringify({
+        //             message: "api route",
+        //             path: req.url
+        //         })
+        //     )
+        // }
 
-            // listen for data chunk
-            req.on('data', chunk => {
-                body += chunk.toString();
-            });
+        // if (req.url == '/api/user' && req.method == "POST") {
 
-            req.on('end', () => {
-                try {
-                    const parsedBody = JSON.parse(body);
-                    console.log('data parsed', parsedBody);
-                    res.end(
-                        JSON.stringify({
-                            ...parsedBody,
-                            path:req.url
-                        })
-                    )
-                }
-                catch(err:any){
-                    console.error(err.message);
-                }
-            });
+        //     let body = '';
+
+        //     // listen for data chunk
+        //     req.on('data', chunk => {
+        //         body += chunk.toString();
+        //     });
+
+        //     req.on('end', () => {
+        //         try {
+        //             const parsedBody = JSON.parse(body);
+        //             console.log('data parsed', parsedBody);
+        //             res.end(
+        //                 JSON.stringify({
+        //                     ...parsedBody,
+        //                     path: req.url
+        //                 })
+        //             )
+        //         }
+        //         catch (err: any) {
+        //             console.error(err.message);
+        //         }
+        //     });
 
 
-        }
+        // }
 
     }
 );
