aacddffd91562ff277f71da8b407f23f5ce520c8
Mysterio-O <skrabbi.019@gmail.com>
2025-11-28 16:41:00 +0600

    added delete user api

--- basic server with raw node/src/helpers/fileDB.ts
+++ basic server with raw node/src/helpers/fileDB.ts
@@ -12,3 +12,20 @@ export function readUsers() {
 export function writeUsers(users: any) {
     fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
 }
+
+export function deleteUser(userId: string) {
+    if (!userId) return null;
+
+    try {
+        const users = readUsers();
+
+        const filteredUsers = users.filter((user: any) => user.id !== userId)
+
+        writeUsers(filteredUsers);
+        return { success: true, message: 'user deleted' }
+    } catch (err) {
+        console.error('error deleting user', err);
+        return { success: false, error: err };
+    }
+
+}
